c827815b51fe21a33cee26ede6ca612762d91ddd
Alex <el.zingo@gmail.com>
2017-02-11 12:40:25 +0200

    Add gpos git alias to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 fish_vi_key_bindings
 
 alias vi="vim"
-alias co="git checkout"
 alias hrc="heroku run rails console"
 alias be="bundle exec"
+
+alias co="git checkout"
+alias gpos="git push origin (git rev-parse --abbrev-ref HEAD)"
