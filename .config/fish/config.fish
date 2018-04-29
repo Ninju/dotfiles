e8a28ab89b84d0297bcbc42e74706e5d926d3302
Alex <alex.watt@fundingcircle.com>
2018-04-29 13:55:48 +0100

    Remove fish config

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-fish_vi_key_bindings
-
-alias vi="vim"
-alias hrc="heroku run rails console"
-alias be="bundle exec"
-
-alias co="git checkout"
-alias gpos="git push origin (git rev-parse --abbrev-ref HEAD)"
