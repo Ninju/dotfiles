1fc5555d0fa8cba88406cdc09d1583e425ee40be
Alex <alex.watt@fundingcircle.com>
2018-05-13 12:05:51 +0100

    Emacs setup uses full stop output hooks
    Makes progress more readable

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
+# full-stop provides a way for us to hook into it's output functions
+source "$1"
+
+header "Installing emacs"
+
 if [ -z "$SPACEMACS_DIR" ]; then
-  echo >&2 SPACEMACS_DIR must be set
+  fail "SPACEMACS_DIR must be set"
   exit 1
+else
+  success "SPACEMACS_DIR is set"
 fi
 
-git clone -b develop git@github.com:syl20bnr/spacemacs.git "$SPACEMACS_DIR"
-ln -s "$SPACEMACS_DIR" ~/.emacs.d
+run_without_failing () {
+  success_message=$1
+  failure_output=$(eval $2 2>&1 1> /dev/null)
+
+  if [ -z "$failure_output" ]; then
+    success "$success_message"
+  else
+    info "$failure_output"
+  fi
+}
+
+run_without_failing "Cloned spacemacs" \
+                    'git clone -b develop git@github.com:syl20bnr/spacemacs.git "$SPACEMACS_DIR"'
+
+run_without_failing "Linked spacemacs to ~/.emacs.d" \
+                    'ln -s "$SPACEMACS_DIR" ~/.emacs.d'
 
 exit 0
