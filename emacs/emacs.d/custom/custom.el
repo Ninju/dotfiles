35462198534191818821f11b3570b63b8482e812
Alex <alex.watt@fundingcircle.com>
2018-06-16 16:46:05 +0100

    Switch modeline color on INS/COMMAND mode (emacs)

--- emacs/emacs.d/custom/custom.el
+++ emacs/emacs.d/custom/custom.el
@@ -79,3 +79,20 @@
 
 ;;- Helm
 (global-set-key (kbd "M-x") 'helm-M-x)
+
+;;- Evil
+;; change mode-line color by evil state
+(defun switch-modeline-color-on-insert-command-mode ()
+  (let* ((default-color (cons (face-background 'mode-line)
+			      (face-foreground 'mode-line)))
+
+	 (color (cond ((minibufferp) default-color)
+		      ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+		      ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+		      ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+		      (t default-color))))
+
+    (set-face-background 'mode-line (car color))
+    (set-face-foreground 'mode-line (cdr color))))
+
+(add-hook 'post-command-hook 'switch-modeline-color-on-insert-command-mode)
