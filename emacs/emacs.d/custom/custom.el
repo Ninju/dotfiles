19c42266171ab623c82c4a10ad1eb0985afa318a
Alex <alex.watt@fundingcircle.com>
2018-06-16 16:44:27 +0100

    Add config for hs-minor-mode/code folding; (emacs)

--- emacs/emacs.d/custom/custom.el
+++ emacs/emacs.d/custom/custom.el
@@ -58,6 +58,18 @@
 ;; Enable auto complete for Ruby
 (add-hook 'ruby-mode-hook 'auto-complete-mode)
 
+;;- hs-minor-mode (code folding)
+(eval-after-load "hideshow"
+  '(add-to-list 'hs-special-modes-alist
+    `(ruby-mode
+      ,(rx (or "def" "class" "module" "do" "{" "[")) ; Block start
+      ,(rx (or "}" "]" "end"))                       ; Block end
+      ,(rx (or "#" "=begin"))                        ; Comment start
+      ruby-forward-sexp nil)))
+
+(global-set-key (kbd "C-c h") 'hs-hide-block)
+(global-set-key (kbd "C-c s") 'hs-show-block)
+
 ;;- Winner mode
 (when (fboundp 'winner-mode)
   (winner-mode 1))
