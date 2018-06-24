0a1711a703dc3c6751300d38780c44b1063c5b34
Alex Watt <el.zingo@gmail.com>
2018-06-25 00:06:11 +0300

    Remove marmalade as a package source (I never use it)

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -6,7 +6,6 @@
 (package-initialize)
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")
 			 ("org" . "http://orgmode.org/elpa/")))
 (custom-set-variables
