a2ea856dfeac6ca89e4ab2049708022555798fdd
Alex <alex.watt@fundingcircle.com>
2019-03-22 11:13:52 +0000

    Configure ace-window

--- emacs/emacs.d/custom/custom.el
+++ emacs/emacs.d/custom/custom.el
@@ -229,3 +229,6 @@
   (setq dired-sidebar-theme 'vscode)
   (setq dired-sidebar-use-term-integration t)
   (setq dired-sidebar-use-custom-font t))
+
+(global-set-key (kbd "C-x o") #'ace-select-window)
+(global-set-key (kbd "C-x w w") #'ace-swap-window)
