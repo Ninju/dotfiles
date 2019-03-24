3777c539a6123b42092c00bf73449d27914ae14f
Ninju <alex@localhost.localdomain>
2019-03-24 14:47:05 +0000

    Emacs: add Elixir LSP support with DAP mode

---
+++ emacs/emacs.d/pkg/pkg-lsp.el
@@ -0,0 +1,27 @@
+(use-package lsp-mode
+  :ensure t
+  :commands lsp
+  :hook
+  (elixir-mode . lsp)
+  :custom
+  (lsp-auto-guess-root t)
+  :init
+  (add-to-list 'exec-path "~/src/downloads/elixir-ls")
+  :config
+  (evil-define-key 'normal lsp-mode-map ",," 'lsp-find-definition)
+  (evil-define-key 'normal lsp-mode-map ",." 'pop-tag-mark)
+  )
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :after lsp-mode
+  )
+
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp
+  :after lsp-mode
+  )
+
+(provide 'pkg-lsp)
