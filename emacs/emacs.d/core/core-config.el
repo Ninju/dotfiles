544a96e44e7c44af45d48617bd5b45630746e231
Ninju <alex@localhost.localdomain>
2019-03-24 14:45:17 +0000

    Emacs: Set backup dir to outside of working dir

--- emacs/emacs.d/core/core-config.el
+++ emacs/emacs.d/core/core-config.el
@@ -1,5 +1,5 @@
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying t    ; Don't delink hardlinks
+(setq backup-directory-alist '(("~/.emacs.d/backup" . "/tmp/emacs-backup"))
+      backup-by-copying t    ; Don't delink hardlink
       version-control t      ; Use version numbers on backups
       delete-old-versions t  ; Automatically delete excess backups
       kept-new-versions 20   ; how many of the newest versions to keep
