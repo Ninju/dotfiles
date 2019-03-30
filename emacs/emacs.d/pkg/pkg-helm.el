f32cc22c83dfd680527f70c34ee908ee675d5c6d
Alex <alex.watt@fundingcircle.com>
2019-03-30 10:51:11 +0000

    Congigure Helm using :general

--- emacs/emacs.d/pkg/pkg-helm.el
+++ emacs/emacs.d/pkg/pkg-helm.el
@@ -1,20 +1,40 @@
 (use-package helm
   :ensure t
-  :bind (
-	("M-x" . helm-M-x)
-	("C-c a s s" . helm-swoop)
-	("C-c a s p" . helm-multi-swoop-projectile)
-	("C-c a h f" . helm-projectile-find-file)
-	("C-c a h TAB" . helm-imenu-in-all-buffers)
-	("C-c p f" . helm-projectile-find-file)
-	("C-c p p" . helm-projectile-switch-project)
-	("C-c C-i" . helm-imenu)
+  :demand t
+  :bind
+  ("M-x" . helm-M-x)
+  ("C-c , i m" . helm-imenu-in-all-buffers)
+  ("C-c , i i" . helm-imenu)
 
-	:map evil-normal-state-map
-	("C-p" . helm-projectile-find-file-dwim)
-	(", TAB" . helm-imenu)
-	(", q" . helm-browse-project)
-	 )
+  ("C-c , h s" . helm-swoop)
+  ("C-c , h q" . helm-browse-project)
+
+  ("C-x b" . helm-buffers-list)
+  :general
+  (:states 'normal
+	   ", i m" 'helm-imenu-in-all-buffers
+	   ", i i" 'helm-imenu
+
+	   ", h s" 'helm-swoop
+	   ", h q" 'helm-browse-project
+
+	   ", b l" 'helm-buffers-list
+	   ", w" 'helm-buffers-list)
   :config
   (setq-default helm-split-window-default-side 'right)
+  )
+
+(use-package helm-projectile
+  :after projectile
+  :ensure t
+  :demand t
+  :bind
+  ("C-c p p" . helm-projectile-switch-project)
+  ("C-c p f" . helm-projectile-find-file)
+  ("C-c p m" . helm-multi-swoop-projectile)
+  :general
+  (:states 'normal
+	   ", p p" 'helm-projectile-switch-project
+	   ", p f" 'helm-projectile-find-file
+	   ", p m" 'helm-multi-swoop-projectile)
   )
