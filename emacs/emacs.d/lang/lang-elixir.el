edc762d755c62911585b08b472f5fd173fe0f7f2
Alex <alex.watt@fundingcircle.com>
2019-03-30 10:50:12 +0000

    Configure Elixir using :general

--- emacs/emacs.d/lang/lang-elixir.el
+++ emacs/emacs.d/lang/lang-elixir.el
@@ -5,15 +5,14 @@
   :defer t
   :ensure t
   :requires (evil exunit)
-  :config
-  (evil-define--key evil-normal-state-map 'exunit-mode-map
+  :general
+  (:states 'normal
     ", t a" 'exunit-verify-all
     ", t v" 'exunit-verify
     ", t r" 'exunit-rerun
-    ", t s" 'exunit-verify-single
-    )
-  ;; (evil-define-key 'normal 'exunit-mode-map (kbd ", t a") 'rspec-verifiable-mode-keymap)
-
+    ", t s" 'exunit-verify-single) ;; I need to make sure this is only in the correct minor mode
+  :config
+  ;; (evil-define--key evil-normal-state-map 'exunit-mode-map
   (require 'dap-elixir)
   (dap-mode)
   (dap-ui-mode)
