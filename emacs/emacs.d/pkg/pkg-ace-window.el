85715cd53f6e5ab7758d26fe9198d712fc9ecdf7
Alex <alex.watt@fundingcircle.com>
2019-03-30 10:52:34 +0000

    Configure ace-window using :general

--- emacs/emacs.d/pkg/pkg-ace-window.el
+++ emacs/emacs.d/pkg/pkg-ace-window.el
@@ -1,7 +1,13 @@
 (use-package ace-window
   :ensure t
-  :bind (
-	 ("C-x o" . ace-select-window)
-	 ("C-x w w" . ace-swap-window)
-	 )
+  :after general
+  :bind
+  ("C-x o" . ace-select-window)
+  ("C-x w s" . ace-swap-window)
+  :general
+  (:states 'normal
+	   ", x o" 'ace-select-window
+	   ", x s" 'ace-swap-window)
   )
+
+(provide 'pkg-ace-window)
