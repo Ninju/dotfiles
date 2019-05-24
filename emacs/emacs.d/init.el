a8446ad309bbc6bf77e4700c4f7d0036b9962ab3
Alex Watt <a-alex.watt@rvu.co.uk>
2019-05-24 09:15:11 +0100

    Don't load solarized in init.el

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -15,7 +15,6 @@
  ;; If there is more than one, they won't work right.
  '(ag-ignore-list (quote ("**.min.js")))
  '(auth-source-save-behavior nil)
- '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes t)
  '(dap-ruby-debug-program
    (quote
@@ -30,7 +29,7 @@
  '(lsp-prefer-flymake :none)
  '(package-selected-packages
    (quote
-    (cider-repl evil-cleverparens org-agenda general magit pipenv poly-R R circleci build-status rspec-mode rainbow-delimeters rainbow-delimeters-mode ace-jump ace-jump-mode exunit dap-ui-mode dap dap-mode lsp company-mode winner-mode ace-window ibuffer-projectile dired-sidebar ibuffer-sidebar ibuffer-vc noflet color-theme-buffer-local load-theme-buffer-local smart-shift highlight-indent-guides flymake-yaml yaml-imenu yaml-mode yaml-tomato rbenv go-mode rainbow-delimiters rainbow-mode company-ghc haskell-mode helm-hoogle scion company-lsp lsp-mode lsp-ocaml lsp-ui reason-mode cider cider-eval-sexp-fu sotclojure merlin merlin-eldoc tuareg web-mode treemacs alchemist elixir-mode s popup pkg-info helm-core flycheck epl async yasnippet company-c-headers objc-font-lock xclip exec-path-from-shell ecb undo-tree company w3m which-key ag helm helm-ag helm-flx helm-fuzzier helm-fuzzy-find helm-projectile helm-swoop projectile evil)))
+    (clj-refactor cider-repl evil-cleverparens org-agenda general magit pipenv poly-R R circleci build-status rspec-mode rainbow-delimeters rainbow-delimeters-mode ace-jump ace-jump-mode exunit dap-ui-mode dap dap-mode lsp company-mode winner-mode ace-window ibuffer-projectile dired-sidebar ibuffer-sidebar ibuffer-vc noflet color-theme-buffer-local load-theme-buffer-local smart-shift highlight-indent-guides flymake-yaml yaml-imenu yaml-mode yaml-tomato rbenv go-mode rainbow-delimiters rainbow-mode company-ghc haskell-mode helm-hoogle scion company-lsp lsp-mode lsp-ocaml lsp-ui reason-mode cider cider-eval-sexp-fu sotclojure merlin merlin-eldoc tuareg web-mode treemacs alchemist elixir-mode s popup pkg-info helm-core flycheck epl async yasnippet company-c-headers objc-font-lock xclip exec-path-from-shell ecb undo-tree company w3m which-key ag helm helm-ag helm-flx helm-fuzzier helm-fuzzy-find helm-projectile helm-swoop projectile evil)))
  '(solarized-high-contrast-mode-line nil)
  '(yas-snippet-dirs (quote ("/Users/alex/.emacs.d/snippets"))))
 (custom-set-faces
@@ -77,5 +76,3 @@
 
 ;; Every time a window is started, make sure it get maximized
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(load-theme 'solarized-dark t)
