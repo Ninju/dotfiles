8666d940e5f8bec49ca47583141c5de6d502b8de
Alex Watt <alex@watt.ventures>
2020-05-31 04:08:21 +0100

    Add PHP language to emacs

--- doom.d/init.el
+++ doom.d/init.el
@@ -142,7 +142,7 @@
        ;;ocaml             ; an objective camel
        org               ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
-       ;;php               ; perl's insecure younger brother
+       php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
