7cc5d115c45b81779ec30756c0649aa78387cca9
Ninju <alex.watt@kaluza.com>
2020-11-08 17:25:46 +0000

    Some Mac specific config for Emacs

--- doom.d/config.el
+++ doom.d/config.el
@@ -105,3 +105,18 @@
       :n ", a k" #'ag-kill-other-buffers
       :n "SPC" #'avy-goto-char)
 
+(when (featurep 'ns)
+  (defun ns-raise-emacs ()
+    "Raise Emacs."
+    (ns-do-applescript "tell application \"Emacs\" to activate"))
+
+  (defun ns-raise-emacs-with-frame (frame)
+    "Raise Emacs and select the provided frame."
+    (with-selected-frame frame
+      (when (display-graphic-p)
+        (ns-raise-emacs))))
+
+  (add-hook 'after-make-frame-functions 'ns-raise-emacs-with-frame)
+
+  (when (display-graphic-p)
+    (ns-raise-emacs)))
