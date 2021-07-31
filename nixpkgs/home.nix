096b167a4261a420cd9e48c99b46ae66c3121164
Ninju <alex.watt@rvu.co.uk>
2021-08-01 00:15:49 +0100

    nix: Comment out 90% of home.nixpkgs
    Hmm..

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,77 +1,88 @@
-{ config ? null, pkgs ? (import <nixpkgs> {}), rofi ? pkgs.rofi, ... }:
+{ config ? null, pkgs ? (import <nixpkgs> {}), ... }:
 
 let
   # u-tool = import uswitch/u.nix pkgs;
+  lorri = import (fetchTarball {
+    url = "https://github.com/target/lorri/archive/rolling-release.tar.gz";
+  }) { };
+
+  path = with pkgs; lib.makeSearchPath "bin" [ nix gnutar git mercurial ];
 in
 {
 # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.username = "***";
-  home.homeDirectory = "***";
+  home.username = "alex";
+  home.homeDirectory = "/home/alex";
 
   home.file.".lein/profiles.clj".source = ./dotfiles/lein/profiles.clj;
   home.file.".config/rofi/config".source = ./dotfiles/config/rofi/config;
+  # home.file.".i3/config".source = ./dotfiles/i3wm/config;
+  # home.file.".vimrc".source = ./dotfiles/vim/vimrc.symlink;
 
-  home.packages = with pkgs; [
-    ag
-    awscli
-    bat
-    cabal-install
-    csvkit
-    escrotum
-    emacs
-    clojure
-    docker
-    google-chrome
-    pgcli
-    mycli
-    feh
-    maim
-    jq
-    openjdk8
-    leiningen
-    httpie
-    pandoc
-    pavucontrol
-    patchelf
-    binutils-unwrapped
-    python37Packages.libxml2 # xmllint
-    python37Packages.grip # for grip-mode live markdown previews in Emacs
-    python3Full
-    python37Packages.pip
-    qutebrowser
-    mplayer
-    dolphin
-    ranger
-    git
-    fzf
-    kubernetes
-    kustomize
-    usbutils
-    openvpn
-    slack
-    stack
-    # u-tool
-    xclip
-    # firefox-bin
-    telnet
-    tree
-    # tcpdump
+  home.packages = [
+    lorri
+    # pkgs.ruby
+#    ag
+#    awscli
+#    bat
+#    cabal-install
+    # pkgs.csvkit
+#    escrotum
+#    emacs
+#    clojure
+#    docker
+#    # google-chrome
+#    pgcli
+#    mycli
+#    # rofi ;; installed through system packages instead
+#    feh
+#    maim
+#    jq
+#    openjdk8
+#    leiningen
+#    httpie
+    pkgs.pandoc
+#    pavucontrol
+#    patchelf
+#    binutils-unwrapped
+#    python37Packages.libxml2 # xmllint
+#    python37Packages.grip # for grip-mode live markdown previews in Emacs
+#    python3Full
+#    python37Packages.pip
+#    qutebrowser
+#    mplayer
+#    dolphin
+#    ranger
+#    git
+#    fzf
+#    kubernetes
+#    kustomize
+#    usbutils
+#    openvpn
+#    slack
+#    stack
+#    # u-tool
+#    xclip
+#    # firefox-bin
+#    telnet
+#    tree
+#    glibcLocales
+#    # tcpdump
   ];
 
   imports = [
-#     ./imports/autorandr.nix
-#     ./imports/neovim.nix
-#     ./imports/bash.nix
-#     ./imports/session-variables.nix
-#     ./imports/emacs.nix
-#     ./imports/keyboard.nix
-     ./imports/git.nix
-#     ./imports/ssh.nix
-#     ./imports/termite.nix
-#     # ./imports/firefox.nix
-#     ./imports/i3.nix
+    #     ./imports/autorandr.nix
+    ./imports/neovim.nix
+    #     ./imports/bash.nix
+    #     ./imports/session-variables.nix
+    #     ./imports/emacs.nix
+    #     ./imports/keyboard.nix
+    ./imports/git.nix
+    #     ./imports/ssh.nix
+    #     ./imports/termite.nix
+    #     ./imports/firefox.nix
+    #     ./imports/i3.nix
   ];
 
   # Custom software that didn't work through home-manager (on MacOS):
@@ -82,5 +93,29 @@ in
   # - termite (no worky on Darwin)
   # - st (no worky on Darwin)
 
+  # systemd.user.sockets.lorri = {
+  #   Unit = { Description = "lorri build daemon"; };
+  #   Socket = { ListenStream = "%t/lorri/daemon.socket"; };
+  #   Install = { WantedBy = [ "sockets.target" ]; };
+  # };
 
+  # systemd.user.services.lorri = {
+  #   Unit = {
+  #     Description = "lorri build daemon";
+  #     Documentation = "https://github.com/target/lorri";
+  #     ConditionUser = "!@system";
+  #     Requires = "lorri.socket";
+  #     After = "lorri.socket";
+  #     RefuseManualStart = true;
+  #   };
+
+  #   Service = {
+  #     ExecStart = "${pkgs.lorri}/bin/lorri daemon";
+  #     PrivateTmp = true;
+  #     ProtectSystem = "strict";
+  #     WorkingDirectory = "%h";
+  #     Restart = "on-failure";
+  #     Environment = "PATH=${path} RUST_BACKTRACE=1";
+  #   };
+  # };
 }
